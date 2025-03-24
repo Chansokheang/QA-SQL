ee51b52f89baad0652a85fa0918696e8a3dd4368
Chansokheang <icebs770@gmail.com>
2025-03-24 15:34:00 +0900

    update readme file

--- database/setup/get_dbs.sh
+++ database/setup/get_dbs.sh
@@ -5,5 +5,5 @@ wget https://bird-bench.oss-cn-beijing.aliyuncs.com/dev.zip -O dev.zip
 # Unzip folders
 unzip dev.zip
 rm dev.zip
-mv dev_20240627 ../dev_folder
-unzip ../dev_folder/dev_databases.zip -d ../dev_folder
+mv dev_20240627 ../dev_folder2
+unzip ../dev_folder2/dev_databases.zip -d ../dev_folder2
